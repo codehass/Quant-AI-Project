1a67df79c3a9061b19b528462116f9847e080e43
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2026-01-22 12:48:33 +0100

    add btc predictions table

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy.sql import func
+from sqlalchemy.types import DateTime
 from ..db.database import Base
 
 
@@ -10,3 +12,14 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
+
+
+class BTCPrediction(Base):
+    __tablename__ = "btc_predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    predicted_close_t_plus_10 = Column(Float, nullable=False)
+    model_version = Column(Float, nullable=False, server_default="1.1")
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
