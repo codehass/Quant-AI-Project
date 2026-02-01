30be80708dcccfdbb3a74b69328a1e39e6b4f1ac
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-01 21:28:15 +0100

    refactor(silver): implement execution wrapper and chain ML trigger
    - Implemented an execution wrapper to enable dynamic module importing, ensuring code changes are picked up without restarting Airflow.
    - Removed time-based cron schedule in favor of event-driven execution (triggered by Bronze DAG).
    - Added TriggerDagRunOperator to automatically initiate the 'train_btc_price_model' DAG upon completion.

--- airflow/dags/silver_processing_dag.py
+++ airflow/dags/silver_processing_dag.py
@@ -1,9 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime, timedelta
-import sys
-import os
-from spark_jobs.silver_processing import run_silver
 
 default_args = {
     'owner': 'airflow',
@@ -14,19 +12,37 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+# --- WRAPPER FUNCTION ---
+# This forces Airflow to reload the code every time the task runs
+def execution_wrapper():
+    import sys
+    import os
+    # Ensure correct path so Python can find the module
+    sys.path.append(os.path.join(os.environ['AIRFLOW_HOME'], 'spark_jobs'))
+    
+    from spark_jobs.silver_processing import run_silver
+    run_silver()
+
 with DAG(
     'silver_processing_dag',
     default_args=default_args,
     description='Reads Bronze, Transforms, and writes to Postgres',
-    schedule_interval='10,25,40,55 * * * *', # Runs 10 mins after Bronze (offset)
+    schedule_interval=None,
     start_date=datetime(2024, 1, 1),
     catchup=False,
     tags=['silver', 'spark', 'postgres'],
 ) as dag:
 
+    # Use the wrapper here, NOT the direct function
     run_silver_task = PythonOperator(
         task_id='process_silver_layer',
-        python_callable=run_silver
+        python_callable=execution_wrapper
     )
 
-    run_silver_task
+    trigger_ml = TriggerDagRunOperator(
+        task_id='trigger_ml_training',
+        trigger_dag_id='train_btc_price_model',
+        wait_for_completion=False
+    )
+
+    run_silver_task >> trigger_ml
